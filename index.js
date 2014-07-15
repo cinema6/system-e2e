f00f3210471d28d24c086a89dc5a01eb5f3feba7
Scott Bouloutian <scott@c6pc2.(none)>
2014-07-15 15:00:50 -0400

    Create separate reports for each browser. Small changes.

--- index.js
+++ index.js
@@ -7,12 +7,19 @@
 
 var BrowserStack = require( "browserstack" ),
     spawn = require('child_process').spawn,
+    fs = require('fs'),
     Q = require('q'),
     Queue = require('madlib-promise-queue'),
     chain = require('./utils/promise').chain,
     config = require('./test/config');
 
-// Setup the connection to BrowserStack to monitor running running workers
+// Make sure the reporting directory exists, and if not, create it
+fs.mkdir('./reports/', function(error) {
+    console.error('ERROR:', 'cannot create reports directory');
+    process.exit(1);
+});
+
+// Setup the connection to BrowserStack to monitor running workers
 var client = BrowserStack.createClient({
     username: process.env.BROWSERSTACK_USER,
     password: process.env.BROWSERSTACK_KEY
@@ -20,7 +27,7 @@ var client = BrowserStack.createClient({
 
 // Announce the start of the tests
 console.log('Beginning to run E2E tests on BrowserStack');
-console.log('Results will be displayed AFTER the tests have completed');
+console.log('Output from concurrently running tests may be mixed.');
 
 // Parse the BROWSERS environment variable
 var browsers = process.env.BROWSERS || 'firefox, chrome, explorer';
@@ -42,48 +49,48 @@ browserList.forEach(function(browserName) {
 getStatus()
 .then(function(status) {
     // Make sure there are no sessions currently running
-	if(status.running_sessions > 0) {
-		console.error('ERROR: there are sessions currently running in BrowserStack');
-		process.exit(1);
-	}
-	console.log('There is a limit of', status.sessions_limit, 'concurrent sessions.');
+    if(status.running_sessions > 0) {
+        console.error('ERROR: there are sessions currently running in BrowserStack');
+        process.exit(1);
+    }
+    console.log('There is a limit of', status.sessions_limit, 'concurrent sessions.');
     var queue = new Queue(status.sessions_limit);
     return Q.all(browserList.map(function(browserName) {
-    	return queue.ready()
-    		.then(function() {
-    			console.log(browserName.toUpperCase() + ': Starting Tests');
-    			return runTestsForBrowser(browserName);
-    		})
-    		.then(function() {
-    			console.log(browserName.toUpperCase() + ': Tests Complete');
-    			queue.ready();
-    			queue.done();
-    		});
+        return queue.ready()
+            .then(function() {
+                console.log(browserName.toUpperCase() + ': Starting Tests');
+                return runTestsForBrowser(browserName);
+            })
+            .then(function() {
+                console.log(browserName.toUpperCase() + ': Tests Complete');
+                queue.ready();
+                queue.done();
+            });
     }));
 })
 .catch(function(error) {
-	console.error('ERROR:', error);
-	process.exit(1);
+    console.error('ERROR:', error);
+    process.exit(1);
 })
 .done();
 
 // Gets the current status of BrowserStack
 function getStatus() {
-	var deferred = Q.defer();
-	client.getApiStatus(function(error, status) {
-		if(error){
-			deferred.reject(new Error(error));
-		}else{
-			deferred.resolve(status);
-		}
-	});
-	return deferred.promise;
+    var deferred = Q.defer();
+    client.getApiStatus(function(error, status) {
+        if(error){
+            deferred.reject(new Error(error));
+        }else{
+            deferred.resolve(status);
+        }
+    });
+    return deferred.promise;
 }
 
 // Spawn a mocha child process. Resolving this promise means that BrowserStack
 //  is ready for a new test to begin to run.
 function runTestsForBrowser(browserName) {
-	var deferred = Q.defer();
+    var deferred = Q.defer();
     var cmd = 'mocha';
     var args = ['--recursive'];
     var options = {
@@ -93,7 +100,7 @@ function runTestsForBrowser(browserName) {
             BROWSER_NAME: browserName,
             BROWSERSTACK_USER: process.env.BROWSERSTACK_USER,
             BROWSERSTACK_KEY: process.env.BROWSERSTACK_KEY,
-            JUNIT_REPORT_PATH: 'report.xml'
+            JUNIT_REPORT_PATH: ('./reports/' + browserName + '_report.xml')
         }
     };
     var child = spawn(cmd, args, options);
