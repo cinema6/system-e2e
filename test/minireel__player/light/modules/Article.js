a17b33f576e8a491ec8ee58fb43a9a59b7841834
Scott Bouloutian <scott@c6pc2.(none)>
2014-09-10 16:35:45 -0400

    Reload page if it doesn't load correctly.

--- test/minireel__player/light/modules/Article.js
+++ test/minireel__player/light/modules/Article.js
@@ -1,17 +1,43 @@
 module.exports = function(browser) {
     'use strict';
 
+    var self = this,
+        promiseWhile = require('../../../../utils/promise').promiseWhile,
+        splashDisplayed = false;
+
     this.exp = 'e-656ebb63ef2c6d';
 
     this.get = function() {
-        return browser.get('http://demo.cinema6.com/e2e/2014/06/17/light-text/')
-            .then(function() {
-                return browser.sleep(1500);
-            })
-            .then(function() {
-                return browser.executeScript(function() {
-                    window.scrollBy(0, 200);
-                });
-            });
+        return promiseWhile(
+            function() {
+                return !splashDisplayed;
+            },
+            function() {
+                return browser.get('http://demo.cinema6.com/e2e/2014/06/17/light-text/')
+                   .then(function() {
+                       return browser.sleep(1500);
+                    })
+                    .then(function() {
+                        return browser.executeScript(function() {
+                            window.scrollBy(0, 200);
+                        });
+                    })
+                    .then(function() {
+                        return browser.findElement({ css: '.c6embed-' + self.exp });
+                    })
+                    .then(function(element) {
+                        return element.isDisplayed();
+                    })
+                    .then(function(isDisplayed) {
+                        splashDisplayed = isDisplayed;
+                    })
+                    .thenCatch(function(error) {
+                        if(error.name === 'NoSuchElementError') {
+                            
+                        }
+                    });
+            }
+        );
+
     };
 };
